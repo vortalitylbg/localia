0d2ff6aa2845fa9848250beb3147be4619170278
Timothée Charruau <timothee.charruau@icloud.com>
2026-03-14 17:19:10 +0100

    update : correction fichier pour lancer le serveur

--- open_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-wt `
-    new-tab cmd /k "cd /d C:\Users\charr\Desktop\Codage\Sites web\Localify\backend && node index.js" `
-    ; `
-    new-tab cmd /k "cd /d C:\Users\charr\Desktop\Codage\Sites web\Localify\frontend && npm run dev"
---
+++ start_server.bat
@@ -0,0 +1,4 @@
+@echo off
+
+wt new-tab cmd /k "cd /d \"C:\Users\charr\Desktop\Codage\Sites web\Localify\backend\" && node index.js" ^
+   ; new-tab cmd /k "cd /d \"C:\Users\charr\Desktop\Codage\Sites web\Localify\frontend\" && npm run dev"
